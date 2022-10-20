ce07d683b41c1734fe5810eb10b82541e2d564b0
jashwanth <jashwanth@sameersolutions.com>
2022-10-21 02:00:53 +0530

    quote

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-async function getQuote() {
+const actualQoute = document.getElementById('actualquote')
+const author = document.getElementById('quote-author')
+const btnclick=document.getElementById('btn')
 
-    const proxyurl="https://cors-anywhere.herokuapp.com/"
+
+async function getQuote(){
+
+    // const proxyurl="htt ps://cors-anywhere.herokuapp.com/"
     
     const apiurl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
     try {
-        const response = await fetch(proxyurl + apiurl)
+        const response = await fetch( apiurl)
         const data = await response.json()
-        console.log(data)
-        
+        actualQoute.innerText = data.quoteText;
+        author.innerText=data.quoteAuthor
     } catch (error) {
+        getQuote()
         console.log(error,'oops something is error occured');
     }
 }
 
-getQuote()
+
+
+btnclick.addEventListener('click',getQuote)
+
